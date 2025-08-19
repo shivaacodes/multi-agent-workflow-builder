fff31b3f9e4c5d172dd881daefe96a6f4e71e10b
Shiva Sajay <shivasajay007@gmail.com>
2025-08-19 17:47:35 +0530

    changed backend payload to send json data

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,8 +2,21 @@
 
 from fastapi import FastAPI
 from app.routes import auth
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins= [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # or ["*"] to allow all
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Registered Routes
 app.include_router(auth.router)
--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -31,12 +31,12 @@ def signup(user_in: UserCreate, db: Session =Depends(get_db)):
 
 # Login
 @router.post("/login", response_model=Token)
-def login(form_data:OAuth2PasswordRequestForm= Depends(),db: Session= Depends(get_db)):
-    user= db.query(User).filter(User.email== form_data.username).first()
-    if not user or not verify_password(form_data.password, user.hashed_password):
+def login_json(user_in: UserCreate, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.email == user_in.email).first()
+    if not user or not verify_password(user_in.password, user.hashed_password):
         raise HTTPException(status_code=401, detail="Invalid credentials")
     
-    token = create_access_token({"sub":str(user.id)})
+    token = create_access_token({"sub": str(user.id)})
     return {"access_token": token, "token_type": "bearer"}
     
 
